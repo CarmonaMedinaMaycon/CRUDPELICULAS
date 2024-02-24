d7159b544fb7b4a455455065b67eb6f564286f04
CarmonaMedinaMaycon <105668860+CarmonaMedinaMaycon@users.noreply.github.com>
2024-02-24 10:54:40 -0600

    Revert "drag and drop"
    This reverts commit a0fd9d1b2ce86102570e731e7b9e79f3457c9c75.

--- src/main/java/utez/edu/mx/movies/models/genres/GenresRepository.java
+++
@@ -1,8 +0,0 @@
-package utez.edu.mx.movies.models.genres;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utez.edu.mx.movies.models.movies.Movies;
-
-public interface GenresRepository extends JpaRepository<Genres, Long> {
-
-}
--- src/main/java/utez/edu/mx/movies/models/movies/MoviesRepository.java
+++
@@ -1,7 +0,0 @@
-package utez.edu.mx.movies.models.movies;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoviesRepository extends JpaRepository<Movies, Long> {
-
-}
